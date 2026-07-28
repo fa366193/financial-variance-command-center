c0786bdde5122c17beb4841e4b7de71e6e20fce7
Fatima Aguilar <fsaguilar16@gmail.com>
2026-07-28 18:51:16 -0400

    Refine institutional finance visual system

--- static/main.tsx
+++ static/main.tsx
@@ -1,1 +1,1 @@
-import React from"react";import{createRoot}from"react-dom/client";import Home from"../app/page";import"../app/globals.css";createRoot(document.getElementById("root")!).render(<React.StrictMode><Home/></React.StrictMode>);
+import React from"react";import{createRoot}from"react-dom/client";import Home from"../app/page";import"../app/globals.css";import"../app/finance-theme.css";createRoot(document.getElementById("root")!).render(<React.StrictMode><Home/></React.StrictMode>);
